d4b71b287c92351c8246dd927a56ee1458181090
OREOHUB <128484354+JonaSaija@users.noreply.github.com>
2024-04-17 13:19:30 +0200

    update
    big

--- src/components/Welcome/startGame.jsx
+++
@@ -1,7 +0,0 @@
-export default function StartGame() {
-  const menuContent = document.getElementById("menuContent");
-  const gameContent = document.getElementById("gameContent");
-
-  menuContent.style.display = "none";
-  gameContent.style.display = "block";
-}
