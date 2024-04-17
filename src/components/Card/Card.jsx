f3096e8d6e65ed46321159d8df73e08faf4d9a39
OREOHUB <128484354+JonaSaija@users.noreply.github.com>
2024-04-17 13:30:03 +0200

    update

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,35 +1,5 @@
 import "./card.css";
 
-<<<<<<< Updated upstream
-export default function Card({ image, name, value }) {
-  const oldFirstCardValue = value;
-  const oldSecondCardValue = value;
-  const oldThirdCardValue = value;
-
-  console.log(
-    "value of old first card = ",
-    oldFirstCardValue,
-    "value of old second card = ",
-    oldSecondCardValue,
-    "value of old third card = ",
-    oldThirdCardValue
-  );
-
-  return (
-    <div className="card">
-      <img src={image} alt={name} className="card-image" />
-      <h2 className="card-name">{name}</h2>
-      <h3 className="card-value">
-        total points :
-        {Math.round(
-          Number(oldFirstCardValue) +
-            Number(oldSecondCardValue) +
-            Number(oldThirdCardValue)
-        )}
-      </h3>
-    </div>
-  );
-=======
 export default function Card({ image, name, value, oldCard }) {
   if (oldCard === false)
     return (
@@ -46,5 +16,4 @@ export default function Card({ image, name, value, oldCard }) {
         <h2 className="card-name">{name}</h2>
       </div>
     );
->>>>>>> Stashed changes
 }
