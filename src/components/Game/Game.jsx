875e07cfaffb1d60f864ddc5aab8270b2851cb85
OREOHUB <128484354+JonaSaija@users.noreply.github.com>
2024-04-23 10:39:05 +0200

    Revert "Update Game.jsx"
    This reverts commit f5267e902ae4a3ad7ed7d19db638479885677302.

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -17,6 +17,7 @@ export default function Game({ playerTurn, setPlayerTurn }) {
   const [dealerRandomCardIndex, setDealerRandomCardIndex] = useState(null);
 
   //Both
+  const [randomCardIndex, setRandomCardIndex] = useState(null);
   const [wonByBlackJack, setWonByBlackJack] = useState(false);
   const [playerWon, setPlayerWon] = useState(null);
   const [cards, setCards] = useState(cardsData);
@@ -100,6 +101,7 @@ export default function Game({ playerTurn, setPlayerTurn }) {
     }
     setDealersTurn(false);
     setPlayerWon(false);
+
 
     if (!dealersTurn && !playerTurn) {
       //Both player and dealer con
